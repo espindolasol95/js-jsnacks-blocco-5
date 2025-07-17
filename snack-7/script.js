776af7615f5e644d4d362d4c16ac2d53c63c5353
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-17 15:31:21 +0200

    feat(usando find abbiamo preso il secondo studente del array)

--- snack-7/script.js
+++ snack-7/script.js
@@ -19,5 +19,14 @@ const students = [
   },
 ];
 
+
+//in questo caso andiamo a prenedre lo studente con id 2 usando find visto che mi trova
+// il primo elemento que rispetta quella condizione la conzione sarà id===2
+
+const secondoStudente= students.find (studente => studente.id===2);
+console.log(secondoStudente);
+
+
+
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
