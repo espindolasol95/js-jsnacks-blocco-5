271b1038e1d7615f3ea21cfdc7f0828b9ecd5d05
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-17 14:48:16 +0200

    feat( nel quito snack uso filter per andare a prendere solo i numeri pari del array)

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,11 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+
+// qua dobbiamo usare filter per andare nello specifico su certi numeri del array in 
+// questo caso i numeri pari perciò useremmo l'operatore % === 0 che fara una 
+// selezione solo de i numeri pari presente nel array
+const numeriPari=nums.filter(numero => numero % 2 === 0);
+console.log (numeriPari)
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
